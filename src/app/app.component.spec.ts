1d9cc4d12727b5d81788643e11ee32aa35fdd3bd
gveliz90 <gveliz90@bancoestado.cl>
2023-03-29 21:01:33 -0300

    Se realizan cambios en proyecto

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,7 +16,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'componentesVeliz'`, () => {
+  it(`should have as title 'listaVeliz'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
   });
@@ -25,6 +25,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('componentesVeliz app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('listaVeliz app is running!');
   });
 });
